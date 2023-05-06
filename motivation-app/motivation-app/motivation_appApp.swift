fd71765139e0d354859e7eccb2b38d30a5a1a4e4
alijnmerchant21 <amercha1@stevens.edu>
2023-05-07 00:31:34 +0100

    Create a swift project

---
+++ motivation-app/motivation-app/motivation_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  motivation_appApp.swift
+//  motivation-app
+//
+//  Created by Aliasgar Merchant on 5/7/23.
+//
+
+import SwiftUI
+
+@main
+struct motivation_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
